beb984d03e40a3fe4b5e8371f1a64b15d9fcea82
nieyihe <813825509@qq.com>
2017-11-12 14:01:23 +0800

    create project

---
+++ app/src/main/java/me/jiesea/academy/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.jiesea.academy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
